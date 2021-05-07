4006dca28c803c4dec2a685135155c943d1660cf
darckore <darckore@gmail.com>
2021-05-07 19:51:05 +0300

    rudimentary things

--- clib/include/clib/keys.hpp
+++ clib/include/clib/keys.hpp
@@ -2,5 +2,21 @@
 
 namespace clib
 {
+  class key
+  {
+  public:
+    enum class kind : uint8_t
+    {
+      cl_switch,    // -s
+      cl_value,     // -v=something
+      cl_opt_value, // -ov[=something]
+      cl_action,    // can associate a function with this one
+    };
 
+    key() = delete;
+    key(const key&) = delete;
+    key& operator=(const key&) = delete;
+    key(key&&) = default;
+    key& operator=(key&&) = default;
+  };
 }
